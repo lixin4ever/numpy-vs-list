f130c7d8520335edcbdbe6148976e47e0c7f9f83
lixin4ever <lixin4ever@gmail.com>
2016-08-28 09:30:01 +0800

    test time cost of element-wise multiplication operation

--- MAT_MULT.py
+++ MAT_MULT.py
@@ -24,6 +24,22 @@ def MAT_MULT(MAT_A, MAT_B):
         res.append(tmp)
     return res
 
+def MAT_MULT_ELEMENTWISE(MAT_A, MAT_B):
+    p, q = len(MAT_A), len(MAT_B)
+    res = []
+    for i in xrange(p):
+        tmp = []
+        for j in xrange(q):
+            tmp.append(MAT_A[i][j] * MAT_B[i][j])
+        res.append(tmp)
+    return res
+
+def MAT_MULT_ELEMENTWISE_NP(MAT_A, MAT_B):
+    if type(MAT_A) is list:
+        MAT_A = np.array(MAT_A)
+        MAT_B = np.array(MAT_B)
+    return MAT_A * MAT_B
+
 def validate(LIST, NDARRAY):
     res = True
     height, width = NDARRAY.shape
@@ -38,15 +54,16 @@ def validate(LIST, NDARRAY):
 
 print 'begin testing...'
 
-assert validate(LIST=MAT_MULT(MAT_A=int_list_2d_A, MAT_B=int_list_2d_B), NDARRAY=np.dot(int_ndarray_2d_A, int_ndarray_2d_B))
+assert validate(LIST=MAT_MULT_ELEMENTWISE(MAT_A=int_list_2d_A, MAT_B=int_list_2d_B),
+                NDARRAY=MAT_MULT_ELEMENTWISE_NP(MAT_A=int_ndarray_2d_A, MAT_B=int_ndarray_2d_B))
 
 print 'pass the result validation!'
 
-methods = {0: MAT_MULT, 1: np.dot}
+methods = {0: MAT_MULT_ELEMENTWISE, 1: MAT_MULT_ELEMENTWISE_NP}
 
 data = {0: (int_list_2d_A, int_list_2d_B), 1: (int_ndarray_2d_A, int_ndarray_2d_B)}
 
-method_names = {0: 'built-in', 1: 'np'}
+method_names = {0: 'SELF', 1: 'np'}
 
 dt_name = {0: 'list', 1: 'np.ndarray'}
 
